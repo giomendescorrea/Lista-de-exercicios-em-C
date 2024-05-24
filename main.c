56b7c65372a775b8d3519a1488663bb8ab3cbcd7
Giovanna Mendes Corrêa <147007498+giomendescorrea@users.noreply.github.com>
2024-05-24 16:00:21 +0000

    soma de elementos do array

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
-Bem-vindo ao meu repositório!
+#include <stdio.h>
+
+int main() {
+    int tamanho; 
+
+    printf("Digite o tamanho do array desejado: ");
+    scanf("%d", &tamanho);
+
+    // array vai ter o tamanho especificado pelo usuário
+    int array[tamanho];
+
+    // solicito ao usuário para inserir os elementos do array até dar o tamanho do array
+    printf("Digite os elementos do array:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    // calcula a soma dos elementos do array
+    int soma = 0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += array[i];
+    }
+
+    // printa a soma dos elementos
+    printf("A soma dos elementos do array é: %d\n", soma);
+    return 0;
+}
