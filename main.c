4dc4270810e91a7778a5cdd8623a184251dacaac
Giovanna Mendes Corrêa <147007498+giomendescorrea@users.noreply.github.com>
2024-05-25 00:40:22 +0000

    inverter array

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
-Bem-vindo ao meu repositório!
+#include <stdio.h>
+#include <stdlib.h>
+
+int* inverterArray(int *array, int tamanho) {
+  int *arrayInvertido = (int *)malloc(tamanho * sizeof(int));
+  for (int i = 0; i < tamanho; i++) {
+    arrayInvertido[i] = array[tamanho - 1 - i];
+  }
+  return arrayInvertido;
+}
+
+int main() {
+  int tamanho;
+
+  printf("Digite o tamanho do array: ");
+  scanf("%d", &tamanho);
+
+  int *array = (int *)malloc(tamanho * sizeof(int));
+  if (array == NULL) {
+    printf("Erro ao alocar memória para o array.\n");
+    return 1;
+  }
+
+  printf("Digite os elementos do array:\n");
+  for (int i = 0; i < tamanho; i++) {
+    scanf("%d", &array[i]);
+  }
+
+  int *arrayInvertido = inverterArray(array, tamanho);
+
+  printf("Array invertido:\n");
+  for (int i = 0; i < tamanho; i++) {
+    printf("%d ", arrayInvertido[i]);
+  }
+  printf("\n");
+  return 0;
+}
+
